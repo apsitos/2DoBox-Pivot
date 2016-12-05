2e8990e46243956231cdbad3867c2d32480b20cb
Joshua Small <jksmall0631@gmail.com>
2016-12-04 19:33:55 -0700

    create third selenium test for delete button

--- test/index.js
+++ test/index.js
@@ -4,12 +4,20 @@ const webdriver = require('selenium-webdriver');
 const test = require('selenium-webdriver/testing');
 
 test.describe('testing 2dobox', function(){
+
+  test.beforeEach(()=>{
   this.timeout(10000);
+  driver = new webdriver.Builder()
+  .forBrowser('chrome')
+  .build();
+  driver.get('http://localhost:8080');
+  });
+
+  test.afterEach(()=>{
+  driver.quit();
+  });
 
   test.it('should allow me to add a title and description', () => {
-    const driver = new webdriver.Builder().forBrowser('chrome').build();
-
-    driver.get('http://localhost:8080');
 
     const title = driver.findElement({name: 'title-field'});
     const body = driver.findElement({name: 'body-field'});
@@ -25,13 +33,9 @@ test.describe('testing 2dobox', function(){
     }).then((value) => {
       assert.equal(value, 'this is the body');
     });
-    driver.quit();
   });
 
   test.it('should append a card to the page', () => {
-    const driver = new webdriver.Builder().forBrowser('chrome').build();
-
-    driver.get('http://localhost:8080');
 
     const title = driver.findElement({name: 'title-field'});
     const body = driver.findElement({name: 'body-field'});
@@ -54,7 +58,37 @@ test.describe('testing 2dobox', function(){
     }).then((text) =>{
       assert.equal(text, 'this is the body');
     });
+  });
 
-    driver.quit();
+  test.it('should delete a card when x button is pressed', () => {
+
+    const title = driver.findElement({name: 'title-field'});
+    const body = driver.findElement({name: 'body-field'});
+    const saveButton = driver.findElement({name: 'save'});
+
+    title.sendKeys('this is a title');
+    body.sendKeys('this is the body');
+    saveButton.click();
+
+    const titl = driver.findElement({className: 'title-input'}).then((title) => {
+      return title.getText();
+
+    }).then((text) =>{
+      assert.equal(text, 'this is a title');
+    });
+
+    const bod = driver.findElement({className: 'body-input'}).then((body) => {
+      return body.getText();
+
+    }).then((text) =>{
+      assert.equal(text, 'this is the body');
+    });
+
+    const deleteButton = driver.findElement({name: 'delete'});
+    const card = driver.findElement({className: 'new-idea'});
+
+    deleteButton.click().then((card) =>{
+      assert.equal(card, undefined);
+    });
   });
 });
