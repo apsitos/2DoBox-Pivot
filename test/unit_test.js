48ae869ab8319e70498eb390c131a16a917b6e09
Annastasia Psitos <annastasia.psitos@gmail.com>
2016-12-05 00:24:08 -0700

    create unit test file

---
+++ test/unit_test.js
@@ -0,0 +1,16 @@
+const assert = require('chai').assert;
+const idea = require('../lib/2dobox.js')
+
+describe ('Create Idea', function() {
+
+  it ('should be a function', function() {
+    assert.isFunction(CreateIdea);
+  });
+
+  it ('should accept a title', function() {
+    let idea = new CreateIdea('title', 'body')
+    assert.equal(idea.title, 'title');
+  });
+
+  
+})
