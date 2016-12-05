cbbcd578f0c6a7d3ec4bb26e893d4325c1ef9369
Annastasia Psitos <annastasia.psitos@gmail.com>
2016-12-05 10:19:33 -0700

    create more unit tests

--- test/unit_test.js
+++ test/unit_test.js
@@ -8,12 +8,22 @@ describe('CreateIdea', function(){
   });
 
   it ('should accept a title', function() {
-    let idea = new CreateIdea('title', 'body')
+    let idea = new CreateIdea('title', 'body');
     assert.equal(idea.title, 'title');
   });
 
   it('should accept a body', function() {
-    let idea = new CreateIdea('title', 'body')
+    let idea = new CreateIdea('title', 'body');
     assert.equal(idea.body, 'body');
+  });
+
+  it('should have a default quality', function() {
+    let idea = new CreateIdea('title', 'body', 'quality');
+    assert.equal(idea.quality, 'Normal');
+  });
+
+  it('should not be completed when added to the page', function() {
+    let idea = new CreateIdea('title', 'body', 'quality', 'done');
+    assert.equal(idea.done, false);
   });
 });
