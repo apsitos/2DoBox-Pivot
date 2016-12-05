8d4fee02f016c00367920b086d271345d71c0496
Joshua Small <jksmall0631@gmail.com>
2016-12-04 19:38:17 -0700

    add increment and decrement selenium tests

--- test/index.js
+++ test/index.js
@@ -91,4 +91,70 @@ test.describe('testing 2dobox', function(){
       assert.equal(card, undefined);
     });
   });
+
+  test.it('pressing the up arrow should increment the importance of a card', () => {
+
+    const title = driver.findElement({name: 'title-field'});
+    const body = driver.findElement({name: 'body-field'});
+    const saveButton = driver.findElement({name: 'save'});
+
+    title.sendKeys('this is a title');
+    body.sendKeys('this is the body');
+    saveButton.click();
+
+    const titl = driver.findElement({className: 'title-input'}).then((title) => {
+      return title.getText();
+    }).then((text) =>{
+      assert.equal(text, 'this is a title');
+    });
+
+    const bod = driver.findElement({className: 'body-input'}).then((body) => {
+      return body.getText();
+    }).then((text) =>{
+      assert.equal(text, 'this is the body');
+    });
+
+    const incrementButton = driver.findElement({name: 'up-vote'});
+
+    incrementButton.click();
+
+    const incrementValue = driver.findElement({className: 'user-quality'}).then((quality) =>{
+      return quality.getText();
+    }).then((quality) => {
+      assert.equal(quality, 'High');
+    });
+  });
+
+  test.it('pressing the down arrow should decrement the importance of a card', () => {
+
+    const title = driver.findElement({name: 'title-field'});
+    const body = driver.findElement({name: 'body-field'});
+    const saveButton = driver.findElement({name: 'save'});
+
+    title.sendKeys('this is a title');
+    body.sendKeys('this is the body');
+    saveButton.click();
+
+    const titl = driver.findElement({className: 'title-input'}).then((title) => {
+      return title.getText();
+    }).then((text) =>{
+      assert.equal(text, 'this is a title');
+    });
+
+    const bod = driver.findElement({className: 'body-input'}).then((body) => {
+      return body.getText();
+    }).then((text) =>{
+      assert.equal(text, 'this is the body');
+    });
+
+    const decrementButton = driver.findElement({name: 'down-vote'});
+
+    decrementButton.click();
+
+    const decrementValue = driver.findElement({className: 'user-quality'}).then((quality) =>{
+      return quality.getText();
+    }).then((quality) => {
+      assert.equal(quality, 'Low');
+    });
+  });
 });
