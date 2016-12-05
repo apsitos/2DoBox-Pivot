7902f1fba8cee6b83ca0f271189bb2de683d920b
Annastasia Psitos <annastasia.psitos@gmail.com>
2016-12-05 10:13:55 -0700

    create initial unit tests

--- test/unit_test.js
+++ test/unit_test.js
@@ -1,8 +1,19 @@
 const assert = require('chai').assert;
-const idea = require('../lib/constructor.js');
+const CreateIdea = require('../lib/constructor');
 
-describe('Create Idea', function(){
+describe('CreateIdea', function(){
+
   it('should be a function', function(){
     assert.isFunction(CreateIdea);
+  });
+
+  it ('should accept a title', function() {
+    let idea = new CreateIdea('title', 'body')
+    assert.equal(idea.title, 'title');
+  });
+
+  it('should accept a body', function() {
+    let idea = new CreateIdea('title', 'body')
+    assert.equal(idea.body, 'body');
   });
 });
