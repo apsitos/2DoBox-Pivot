2ac68b4c48dd6d1f2a11a9b62ca3f6ab9e3436d0
Joshua Small <jksmall0631@gmail.com>
2016-12-05 09:50:56 -0700

    fix tests and create seperate constructor file

---
+++ test/unit_test.js
@@ -0,0 +1,8 @@
+const assert = require('chai').assert;
+const idea = require('../lib/constructor.js');
+
+describe('Create Idea', function(){
+  it('should be a function', function(){
+    assert.isFunction(CreateIdea);
+  });
+});
